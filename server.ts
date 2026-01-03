8398104a5c061e0284459ce1d67f99a5e1625fd0
ankitaa2603 <ankitaa2603@gmail.com>
2026-01-03 17:22:52 +0530

    Add CORS to server

--- server.ts
+++ server.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
+
 app.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Server is working again!',
+    message: 'Server with CORS working!',
     env: {
       DATABASE_URL: process.env.DATABASE_URL ? 'SET' : 'NOT SET',
       BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET ? 'SET' : 'NOT SET',
