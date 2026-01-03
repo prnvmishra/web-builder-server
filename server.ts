aeed6783001d575a3c647b58b5b223a224f75c6e
ankitaa2603 <ankitaa2603@gmail.com>
2026-01-03 17:19:15 +0530

    Switch back to clean server without dependencies

--- server.ts
+++ server.ts
@@ -1,27 +1,9 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
-import cors from 'cors';
-import { toNodeHandler } from 'better-auth/node';
-import { auth } from './lib/auth.js';
-import userRouter from './routes/userRoutes.js';
-import projectRouter from './routes/projectRoutes.js';
-import { stripeWebhook } from './controllers/stripeWebhook.js';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-
-const corsOptions = {
-    origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
-    credentials: true,
-}
-
-app.use(cors(corsOptions))
-app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
-
-app.all('/api/auth/*', toNodeHandler(auth));
-
-app.use(express.json({limit: '50mb'}))
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
@@ -33,9 +15,6 @@ app.get('/', (req: Request, res: Response) => {
     }
   });
 });
-
-app.use('/api/user', userRouter);
-app.use('/api/project', projectRouter);
 
 // For local development
 if (!process.env.VERCEL) {
